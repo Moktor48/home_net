cd9a8ca71419220fb27dc7cf907f452c7f39ba88
moktor48 <chris.berhow@gmail.com>
2023-10-16 23:43:59 -0700

    Letters shall be yellow!

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
-import Calendar from './Components/calendar'
+import Calendar from './Components/Calendar'
+import TaskEntry from './Components/TaskEntry'
+import AdminEntry from './Components/AdminEntry'
+import TaskList from './Components/TaskList'
 
 export default function Home() {
   return (
     <div>
     <Calendar />
+    <TaskList />
+    <TaskEntry />
+    <AdminEntry />
+
     </div>
   )
 }
